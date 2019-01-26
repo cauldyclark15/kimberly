3643bd3368c6e742c3ec8551bd81b366c767b226
joseliecast <joselie@laguro.com>
2019-01-26 13:35:37 +0800

    yes

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('lunch time na')
+const express = require('express');
+const path = require('path');
+
+const server = express();
+console.log('something');
+
+server.use(express.static(path.join('build')));
+
+server.listen(5000);
+console.log('server running at port 5000');
